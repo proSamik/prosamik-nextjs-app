d1180f1dbdb4b6c34b380e5793068c3af9d79db2
proSamik <samikchoudhury15@gmail.com>
2024-12-22 06:45:15 +0530

    fix- media center alignment in mediaCenterProcessor.ts

--- src/utils/contentProcessors/mediaCenterProcessor.ts
+++ src/utils/contentProcessors/mediaCenterProcessor.ts
@@ -1,16 +1,13 @@
 export const processCenteredMedia = (html: string) => {
     return html.replace(
         /<(img|video|iframe)([^>]*)(title="([^"]*)")?([^>]*)>/g,
-        (match, tag, attrs1, titleAttr, title, attrs2) => {
-            const titleHtml = title
-                ? `<div class="text-center mb-2 font-semibold">${title}</div>`
-                : '';
+        (_, tag, attrs1, titleAttr, attrs2) => {
             return `
-                    <div class="my-4 flex flex-col items-center">
-                        ${titleHtml}
-                        <${tag}${attrs1} ${attrs2} class="max-w-full"${titleAttr ? ` ${titleAttr}` : ''}>
-                    </div>
-                `;
+                <div class="my-4 flex justify-center items-center">
+                    <${tag}${attrs1}${attrs2} class="max-w-full"${titleAttr ? ` ${titleAttr}` : ''}>
+                </${tag}>
+                </div>
+            `;
         }
     );
 };
