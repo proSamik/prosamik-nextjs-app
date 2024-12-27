aae547a4756e74bfa805b94ca58168309fff83e1
proSamik <samikchoudhury15@gmail.com>
2024-12-27 16:55:46 +0530

    refactor- fix the warnings

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { useSubscribeNewsletter } from '@/hooks/useSubscribeNewsletter';
 
 export default function CallToAction() {
