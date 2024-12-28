d82ceca6c96faee15971542e0938ed926a7eb137
proSamik <samikchoudhury15@gmail.com>
2024-12-29 04:25:11 +0530

    feat- update it to include repoPath for ItemCard.tsx

--- src/components/ProjectList.tsx
+++ src/components/ProjectList.tsx
@@ -18,7 +18,8 @@ const ProjectList: React.FC<ProjectListProps> = ({ repos }) => {
         link: `/project/${encodeURIComponent(repo.title)}`,
         description: repo.description,
         tags: repo.tags,
-        views_count: repo.views_count
+        views_count: repo.views_count,
+        repoPath: repo.repoPath  // Important as it distinguish the blog list
     }));
 
     return <ItemList items={items} title="My Projects" />;
