f5e39c563c56f2a3ddfd6800c5bdf9babf5015f0
proSamik <samikchoudhury15@gmail.com>
2024-12-31 19:25:49 +0530

    feat- update the [slug] to use the ContentDetailPage component and writing the type

--- src/pages/project/[slug].tsx
+++ src/pages/project/[slug].tsx
@@ -1,18 +1,5 @@
-import { useRouter } from 'next/router';
-import { useProjectsList } from '@/hooks/useProjectsList';
-import { useRepoHandler } from '@/hooks/useRepoHandler';
-import ProjectLayout from '@/components/ProjectLayout';
-import RepoLoader from '@/components/RepoLoader';
+import ContentDetailPage from '@/components/ContentDetailPage';
 
 export default function ProjectPost() {
-    const router = useRouter();
-    const { slug } = router.query;
-    const { data: repoList, error: repoError, loading: repoLoading } = useProjectsList();
-    const { repoInfo, data, error, loading } = useRepoHandler(slug, repoList?.repos);
-
-    return (
-        <RepoLoader loading={repoLoading || loading} error={repoError || error}>
-            {data && repoInfo && <ProjectLayout data={data} />}
-        </RepoLoader>
-    );
+    return <ContentDetailPage type="project" />;
 }
