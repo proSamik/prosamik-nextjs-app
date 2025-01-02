68d191323699660172c0382ec50e19ed3a68f4fa
proSamik <samikchoudhury15@gmail.com>
2025-01-03 04:49:22 +0530

    feat- update it to use the MainLayout.tsx as a common layout and passed other components as PageProps

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,32 @@
+// pages/_app.tsx
 import type { AppProps } from 'next/app'
 import '@/styles/globals.css'
+import MainLayout from '@/components/MainLayout'
+import { usePageAnalytics } from "@/hooks/usePageAnalytics";
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }: AppProps) {
+    const { trackPageView } = usePageAnalytics();
+    const router = useRouter();
+
+    useEffect(() => {
+        // Track page views whenever the route changes
+        const handleRouteChange = (url: string) => {
+            const pageName = url.split('/')[1] || 'home';
+            void trackPageView(pageName);
+        };
+
+        router.events.on('routeChangeComplete', handleRouteChange);
+
+        return () => {
+            router.events.off('routeChangeComplete', handleRouteChange);
+        };
+    }, [router.events, trackPageView]);
+
     return (
-        <>
+        <MainLayout>
             <Component {...pageProps} />
-        </>
+        </MainLayout>
     );
 }
