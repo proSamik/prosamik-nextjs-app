d09c71ffc7853123da96a1cd0d023948f749410c
proSamik <samikchoudhury15@gmail.com>
2025-01-03 02:09:21 +0530

    fix- remove SEO from _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,9 @@
 import type { AppProps } from 'next/app'
 import '@/styles/globals.css'
-import SEO from "@/components/layout/SEO";
-import {siteMetadata} from "@/utils/siteMetadata";
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <>
-            <SEO
-                title={siteMetadata.title}
-                description={siteMetadata.defaultDescription}
-            />
             <Component {...pageProps} />
         </>
     );
