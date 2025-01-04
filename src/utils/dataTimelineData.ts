0f7f94c59ff63e75a314a1368f800c45c43eda98
proSamik <samikchoudhury15@gmail.com>
2025-01-05 00:55:45 +0530

    data- add data for year 2023-24

--- src/utils/dataTimelineData.ts
+++ src/utils/dataTimelineData.ts
@@ -4,7 +4,8 @@ export const dataTimelineData: TimePeriod[] = [
   {
     yearRange: {
       start: "2024",
-      end: "Present"
+      end: "Present",
+      description: "Rough phase"
     },
     events: [
       {
@@ -22,18 +23,29 @@ export const dataTimelineData: TimePeriod[] = [
   {
     yearRange: {
       start: "2023",
-      end: "2024"
+      end: "2024",
+      description: "Year to have fun"
     },
     events: [
       {
-        title: "DevOps Trainee @rtCamp",
-        description: "Learned and implemented DevOps practices, cloud infrastructure, and automation techniques.",
-        skills: ["#DevOps", "#AWS", "#Docker", "#GitLab"]
+        title: "On-Campus Placement",
+        description: "- Before starting out, I took a timeline that I had to get placed in the month of August 2023, and that’s how it all started.\n- Started thinking that the first company which would be coming in that I would be placed because I worked tirelessly and gave my all in the past few years.\n- The D arrived and didn’t portray myself in the best way possible.\n- Rejected at many steps, and when I reached the interview stages, I think these could be the reasons for getting rejections:\n\t- ZS Associates (applied for DAA role and gave an interview like a tech person)\n\t- Mastercard (Round 2 technical, got too nervous when I couldn’t explain a lot)\n\t- Transunion (even I don’t know the reason)\n- 3 times in a row rejection that also occurred within 1 week, compelled me to get placed somehow in the next interview.\n\t- Got placed in Varasi (only one in the college)\n\t- Because it was a Salesforce Dev role job, the bar of competition was low, and evaluating now, even I don’t know how I convinced myself for that role because I had no previous work experience in that field.\n\t- Rs. 8 Lakhs in-hand yearly package with Rs. 12,000 internet cost. (CTC was also 8 Lakhs only)\n\t- The initial joining date was July 2024, but due to business challenges, they shifted the date to Jan 2025.",
+        skills: ["#CampusPlacement", "#InterviewPreparation", "#SalesforceDeveloper", "#Varasi", "#DBMS", "Java" ]
       },
       {
-        title: "Tech Community Leadership",
-        description: "Led multiple tech communities, organized workshops and hackathons, fostering collaboration and learning.",
-        skills: ["#Leadership", "#Community", "#EventManagement"]
+        title: "Off-Campus Placement",
+        description: "- As I was not satisfied with my previous placements, I looked for new opportunities in different roles and luckily got an interview call from rtCamp (and that’s how it all started).\n- To be honest, I didn’t know about so many concepts of web development before that day, so I looked for rtCamp interview questions on Glassdoor (I looked on Google before every interview) and started preparing the topics.\n- And again, luckily and with a mix of right and wrong answers in the interview, I got selected at rtCamp (Sep 2024).\n- The joining date was Apr ’24 (6 months probation for Rs. 50,000 monthly and in-hand Rs. 1 Lakh monthly offer post probation).\n- Later on, the date shifted to July ’24.\n- And once I got this opportunity, I stopped applying to any other company and left development altogether and focused mainly on BE project and on my health.",
+        skills: ["#OffCampusPlacement", "#WebDevelopment", "#InterviewPreparation", "#React", "#rtCamp"]
+      },
+      {
+        title: "Final Year Project",
+        description: "- I was making this project from my second year through PBL and mini projects.\n- It was a Smart Parking System using IoT. My vision with this project was to automate parking because in India, we have a lot of parking problems and a lot of human effort goes into parking management.\n- Since I was building this project only for the project and not for shipping, it never came as a foolproof plan; it was always only upto the requirements and nothing else above that.",
+        skills: ["#IoTProjects", "#SmartParkingSystem", "#FinalYearProject", "#ProblemSolving", "#EngineeringProjects"]
+      },
+      {
+        title: "Lost Weight: 96 kgs to 70 kgs",
+        description: "- I did all this in a span of 5 months. As I told you, I had some timeline for placements and didn’t do any development, so I thought it was time to take care of my body and started working on that.\n- Gym never appealed to me a lot before, so I took up other sports for that where I enjoyed the process also. So, I learnt Squash (as it was less competitive and crowded in college as it is an expensive game and requires maintenance).\n- Once I became good at Squash, it was hard to convince others to play this game who were at my level (because I played terribly in it). So, I improved my game a lot due to that, but I was still behind many players.\n- So, to keep the fun going, I started playing badminton, which was very competitive and crowded, but I cracked the timing and saw that in the morning (4 a.m. to 6 a.m.) it used to be empty and post 9 p.m. in the night it used to be empty. So, I started playing in those slots and the rest of the time I dedicated to Squash. I was all day playing and trying new sports.\n- I used to walk around 10,000 steps daily in college. Evening walks with friends were the amazing part.\n- I also started running in the last few months of college days (avg 5:40 mins/km) and recorded each run on Strava to increase my agility in the game because again I was terrible at badminton too, so increasing the stamina and agility gave me some edge to judge opponents and act on them easily.\n- I also went on cycling rides during the weekend, and it was always more than 20 kms. The longest I did in college was 80 kms. With all the running effort, cycling was easy for me.\n- I did utilize my time to the fullest and gave focus on one single goal, and that was to enjoy as much as possible in this journey.\n- And on that note, that’s how my 4 years of college were completed.",
+        skills: ["#WeightLossJourney", "#FitnessTransformation", "#HealthyLifestyle", "#SportsFitness", "#CollegeLife"]
       }
     ]
   },
