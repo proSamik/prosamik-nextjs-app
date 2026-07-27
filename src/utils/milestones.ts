23a6d690fab4ca76d71785caf22ed5859c7a0181
proSamik <samikchoudhury15@gmail.com>
2026-07-27 17:12:30 +0530

    feat: Enhance milestones utility with year-based retrieval
    - Added a new `milestoneYears` export to structure milestone data by year, improving data organization.
    - Introduced `getMilestoneYear` function to retrieve milestones based on the year, enhancing accessibility to year-specific milestone information.

--- src/utils/milestones.ts
+++ src/utils/milestones.ts
@@ -1,6 +1,12 @@
 import { dataTimelineData } from '@/utils/dataTimelineData';
 import { getMilestoneSlug, getYearSlug } from '@/utils/slugs';
 
+export const milestoneYears = dataTimelineData.map((period) => ({
+    year: getYearSlug(period.yearRange.start, period.yearRange.end),
+    yearRange: period.yearRange,
+    events: period.events,
+}));
+
 export const milestones = dataTimelineData.flatMap((period) =>
     period.events.map((event) => ({
         year: getYearSlug(period.yearRange.start, period.yearRange.end),
@@ -12,4 +18,8 @@ export const milestones = dataTimelineData.flatMap((period) =>
 
 export function getMilestone(year: string, milestone: string) {
     return milestones.find((item) => item.year === year && item.slug === milestone);
+}
+
+export function getMilestoneYear(year: string) {
+    return milestoneYears.find((item) => item.year === year);
 }
